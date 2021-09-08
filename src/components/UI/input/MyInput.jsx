b3345197940f41e6f70d283a10d1d4def92d4b11
Yaroslavom <yarykomm@gmail.com>
2021-09-09 01:08:44 +0300

    Refactoring Post state

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from "./MyInput.module.css"
 
-const MyInput = React.forwardRef((props, ref) => {
+const MyInput = (props) => {
     return (
-        <input ref={ref} className={classes.myInput} {...props}/>
+        <input className={classes.myInput} {...props}/>
     )
-});
+};
 
 export default MyInput
